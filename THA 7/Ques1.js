200403d59a346d4069e7a01924dedece0b8535b7
Himanshi_761 <himanshimishra67@gmail.com>
2021-06-15 21:36:40 +0530

    Javascript Objects

---
+++ THA 7/Ques1.js
@@ -0,0 +1,4 @@
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+for (var x in student) {
+    console.log(x)
+}
---
+++ THA 7/Ques5.js
@@ -0,0 +1,8 @@
+var Volume = {
+    radius: 7,
+    length: 4
+};
+var r = Volume.radius;
+var l = Volume.length;
+var vol = (3.14) * r * r * l;
+console.log(vol.toFixed(4));
