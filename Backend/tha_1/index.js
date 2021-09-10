1e040dbfa89950dfac32b450b5a773ca78a459aa
Himanshi Mishra <63972876+himanshi-761@users.noreply.github.com>
2021-09-10 23:08:18 +0530

    Delete Backend/tha_1 directory

--- Backend/tha_1/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-fs.mkdirSync("tha_1");
-fs.writeFileSync("tha_1/input.txt", "I love instagram");
-fs.appendFileSync("tha_1/input.txt", "I am learning Node.js");
-
-var obj = new test();
-console.log(obj.print());
--- Backend/tha_1/require_test.js
+++
@@ -1,6 +0,0 @@
-class test {
-    print() {
-        console.log("I am require_test")
-    }
-}
-module.exports = test;
